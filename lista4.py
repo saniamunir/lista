c9ec21bc18db94faebbf37a0fbcc9f6b4db31d49
saniamunir <35691533+saniamunir@users.noreply.github.com>
2018-03-05 13:04:46 +0100

    Create lista4.py

---
+++ lista4.py
@@ -0,0 +1,27 @@
+# coding: utf-8
+numero = int(input("Dígame cuántas palabras tiene la lista: "))
+
+if numero < 1:
+    print("¡Imposible!")
+else:
+    lista = []
+    for i in range(numero):
+        print("Dígame la palabra:")
+        palabra = input()
+        lista += [palabra]
+    print("La lista creada es:",["Carmen","Alberto","Benito","Carmen"])
+
+limite=input("voltes")
+while limite<=0
+ limite=input("Palabra a eliminar:")
+llista=[]
+for i in range(1,llista+1):
+ paravla=raw_input("  ")
+ llista=+paravla
+print llista
+eliminar=raw_input("paravla a eliminar:")
+llista2=[ ]
+for i in llista
+ if i eliminar
+ llista= append(i)
+print llista2
